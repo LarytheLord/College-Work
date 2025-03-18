326b76b0623ace609eefb3f47f214675434bd5db
Abid Khan <llawlietbagsum@gmail.com>
2025-03-18 10:46:42 +0530

    18/03/2025

---
+++ ABI10.C
@@ -0,0 +1,22 @@
+//wacp to read a number from the user and print corrosponding day
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int z,m;
+	a=mon;
+	b=tus;
+	c=wed;
+	d=thu;
+	e=fri;
+	f=sat;
+	g=sun;
+	clrscr();
+	printf("Assuming that 1st of January is Monday\n");
+	printf("Enter your month=\n");
+	scanf("%d",m);
+	if(m==1 && m==3 && m==5 && m==7 && m==8 && m==10 && m=12);
+	printf("Enter the number between 1 to 31\n");
+	scanf("%d")
+	printf("Enter your desiered number for the day between 1 to 31\n");
+	scanf("%d",z);
+	if()
