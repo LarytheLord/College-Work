7995126e2ad8a45f8b3f61a7fc1f1ba34e0ee118
Abi <llawlietbagsum@gmail.com>
2025-08-18 15:03:23 +0530

    Update LINKABI0.C

--- LINKABI0.C
+++ LINKABI0.C
@@ -1,43 +1,80 @@
-#include<conio.h>
-#include<stdio.h>
-#include<stdlib.h>
-struct node{
-int data;
-struct node *next;
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the node structure
+struct node {
+    int data;
+    struct node *next;
 };
+
+// Head pointer for the linked list
 struct node *head = NULL;
 
-struct node* createnode(int v){
-struct node* newnode = (struct node*)
-malloc(sizeof(struct node));
-if(newnode == null){
-printf("\n Memory not allocate");
-}
-newnode-> data = v;
-newnode-> next = null;
-return newnode;
-}
-void insertbeg(int v){
-struct node *newnode = createnode(v);
-newnode-> next = head;
-head = newnode;
-}
-void display(){
-struct node *temo = head;
-if (temp != null){
-printf("\n The list is empty");
-while(temp !=null);
-printf("%d", temp->data);
-}
-}
+// Function to create a new node
+struct node* createnode(int v) {
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    if (newnode == NULL) {
+        printf("\nMemory not allocated");
+        exit(1); // Exit on memory allocation failure
+    }
+    newnode->data = v;
+    newnode->next = NULL;
+    return newnode;
 }
 
-void main(){
-int v,ch;
-do{
-printf("\n enter 1:inter at beginning \n 2.display \n 3.exit");
-switch(ch){
-case 1:
-printf("\n enter to value at beginning");
-scanf("%d",&val);
+// Insert a node at the beginning of the list
+void insertbeg(int v) {
+    struct node *newnode = createnode(v);
+    newnode->next = head;
+    head = newnode;
+}
 
+// Display the list
+void display() {
+    struct node *temp = head;
+    if (temp == NULL) {
+        printf("\nThe list is empty\n");
+        return;
+    }
+    printf("\nThe list is: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Main function
+int main() {
+    int val, ch;
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Insert at beginning\n");
+        printf("2. Display list\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1:
+                printf("Enter value to insert at beginning: ");
+                scanf("%d", &val);
+                insertbeg(val);
+                break;
+
+            case 2:
+                display();
+                break;
+
+            case 3:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    } while (ch != 3);
+
+    return 0;
+}
